64ab041de365b0133426afeb1f9dc07fc66f6590
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-01-23 10:32:08 -0500

    Multiple changes
    - Move chat message formatter to a function
    - Read usernames and passwords from a plaintext JSON object in the environment named USERS (currently unused)
    - Fix the welcome/join messages

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+const users = process.env.USERS ? JSON.parse(process.env.USERS) : {"admin": "adminpassword", "user": "userpassword"}
+
+const format_msg = msg => msg.replace("\\\\", "\f") // temp rm \\
+                             .replace("\\r\\n", "\n")
+                             .replace("\\r", "\\n")
+                             .replace("\\n", "<br/>")
+                             .replace("\\t", "\t")
+                             .replace("\f", "\\\\")
+                             .split("<br/>")
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -12,17 +22,9 @@ app.get("/favicon.ico", (req, res) => {
 });
 
 io.on('connection', function(socket){
-  socket.emit(`! Welcome, <${socket.id}>`);
-  socket.broadcast.emit(`! <${socket.id}> has joined.`);
-  socket.on('chat message', function(msg){
-    msg.replace("\\\\", "\f") // temp rm \\
-       .replace("\\r\\n", "\n")
-       .replace("\\r", "\\n")
-       .replace("\\n", "<br/>")
-       .replace("\\t", "\t")
-       .replace("\f", "\\\\")
-       .split("<br/>")
-       .map((m) => {io.emit("chat message", `% <${socket.id}> ${m}`);});
+  socket.emit("chat message", `! Welcome, <${socket.id}>`);
+  socket.broadcast.emit("chat message", `! <${socket.id}> has joined.`);
+  socket.on('chat message', msg => format_msg(msg).map((m) => {io.emit("chat message", `% <${socket.id}> ${m}`);}));
   });
 });
 
