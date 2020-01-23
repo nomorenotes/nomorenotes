11006681f999daab0e4cf4b9bc5e067e981cb2d0
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2020-01-23 14:36:51 -0500

    Implement possible name change support

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const users = process.env.USERS ? JSON.parse(process.env.USERS) : {"admin": "adm
 
 const names = {};
 
+const magic = msg => {
+  switch (msg) {
+    default:
+      return false;
+  }
+}
+
 const format_msg = msg => msg.replace("\\\\", "\f") // temp rm \\
                              .replace("\\r\\n", "\n")
                              .replace("\\r", "\\n")
@@ -27,7 +34,11 @@ io.on('connection', function(socket){
   names[socket.id] = socket.id.slice(0,8);
   socket.emit("chat message", `! Welcome, <${names[socket.id]}>`);
   socket.broadcast.emit("chat message", `! <${names[socket.id]}> has joined.`);
-  socket.on('chat message', msg => format_msg(msg).map((m) => {io.emit("chat message", `% <${names[socket.id]}> ${m}`);}));
+  socket.on('chat message', msg => (
+                                   magic(msg)
+                                   ? format_msg(msg).map((m) => {io.emit("chat message", `% <${names[socket.id]}> ${m}`);})
+                                   : undefined
+                                   ));
   socket.on("disconnect", () => { io.emit("chat message", `! <${names[socket.id]}> has left.`); names[socket.id] = undefined;});
 });
 
