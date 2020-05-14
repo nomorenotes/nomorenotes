729be96d49052d1602b59739d10edb8e12c8b4f9
PoolloverNathan <nathan0kulzer@gmail.com>
2020-05-14 10:51:58 -0400

    Change routes

--- chat/module.js
+++ chat/module.js
@@ -1,11 +1,11 @@
 module.exports = (app) => {
-  app.get('/chat', function(req, res){
+  app.get('/raw_chat', function(req, res){
     res.sendFile(__dirname + '/index.html');
   });
-  app.get("/chat.js", (req, res) => {
+  app.get("/raw_chat.js", (req, res) => {
     res.sendFile(__dirname + "/main.js");
   });
-  app.get("/chat.css", (req, res) => {
+  app.get("/raw_chat.css", (req, res) => {
     res.sendFile(__dirname + "/styles.css");
   });
 }
