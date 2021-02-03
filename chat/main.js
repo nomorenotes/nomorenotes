0066061d41f259044a6dd6b50509bd6ff5aad9b4
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2021-02-03 11:08:14 -0500

    Add much more debugs than neccesarry

--- chat/main.js
+++ chat/main.js
@@ -17,11 +17,13 @@ $(function () {
     hello(socket, localStorage.session ? localStorage.session : (localStorage.session = socket.id), "", "");
   });
   $('#send').submit(function(){
+    debug(`sending message ${$("#m").val()}`);
     socket.emit('chat message', $('#m').val());
     $('#m').val('');
     return false;
   });
   socket.on('chat message', function(id, msg){
+    debug(`recieving message ${id}: ${msg}`); 
     $('#messages').append($('<li>', {id}).html(msg));
     if (notify) {
       notify = manotify;
@@ -30,22 +32,27 @@ $(function () {
     window.scrollTo(0, document.body.scrollHeight);
   });
   socket.on("ping", (wasTargeted, source) => {
+    debug(`recieving ${wasTargeted} ping from ${source}`);
     alert(`${source} has pinged ${wasTargeted ? "you" : "everyone"}!`);
     
   });
   socket.on("edit", (id, msg) => {
+    debug(`editing message ${id} (${$(`#${id}`).text()}) to ${msg}`);
     $(`#${id}`).text(msg);
   });
   socket.on("setStorage", (key, value) => {
+    debug(`storage[${key}] = ${value}`);
     localStorage[key] = value;
   });
   socket.on("delete", (id) => {
+    debug(`deleting message ${id} (${$(`#${id}`).text()})`);
     document.getElementById(id).removeElement();
   });
-  socket.on("reload", ()=>{history.go(0);});
-  socket.on("linkout", (url)=>{open(url);});
+  socket.on("reload", ()=>{debug("reloading");history.go(0);});
+  socket.on("linkout", (url)=>{debug(`linking to ${url}`);open(url);});
   $.on("blur", ()=>{alert("blur");});
   document.getElementById('m').onpaste = function (event) {
+    debug("beginning paste");
   // use event.originalEvent.clipboard for newer chrome versions
   var items = (event.clipboardData  || event.originalEvent.clipboardData).items;
   alert(JSON.stringify(items)); // will give you the mime types
