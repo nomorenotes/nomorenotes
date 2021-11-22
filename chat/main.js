972ab1f9dbd1aac4dee6cb47e7b918ae2f0b4dc0
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2021-11-22 09:47:22 -0500

    Enforce banning

--- chat/main.js
+++ chat/main.js
@@ -1,4 +1,5 @@
 if (location.protocol === "http:") location.protocol = "https:";
+else if (localStorage.banExpiry && +localStorage.banExpiry > Date.now()) location.pathname = "/chat";
 else $(function () {
   
   const saveable = ["name"];
