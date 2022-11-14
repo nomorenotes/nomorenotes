bce820160aa26a5c5a543c117f336e5c2796836d
PoolloverNathan <nathan.kulzer@protonmail.com>
2022-11-14 16:41:17 +0000

    replit dot nix
    Cherry-picked with changes from edeb54d5c6429fd79c7c2dfb26867cc9aefb9ef5

--- replit.nix
+++ replit.nix
@@ -1,13 +1,14 @@
 
 { pkgs }: {
-    deps = [
-        pkgs.vim
-        pkgs.busybox
-        pkgs.gnupg
-        pkgs.heroku
-        pkgs.less
-        pkgs.nodejs-16_x
-			  pkgs.nano
-        pkgs.python3
-    ];
+  deps = [
+    pkgs.busybox
+    pkgs.gnupg
+    pkgs.vim
+    pkgs.screen
+    pkgs.heroku
+    pkgs.less
+    pkgs.nodejs-16_x
+    pkgs.nano
+    pkgs.python3
+  ];
 }
