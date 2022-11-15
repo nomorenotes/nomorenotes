6611e41fbd86515966d6a0134a879e23972ea1da
PoolloverNathan <nathan.kulzer@protonmail.com>
2022-11-15 13:56:05 +0000

    lunchables

--- replit.nix
+++ replit.nix
@@ -1,12 +1,14 @@
 
 { pkgs }: {
   deps = [
+    pkgs.dialog
+    pkgs.jq.bin
+    pkgs.less
     pkgs.busybox
     pkgs.gnupg
     pkgs.vim
     pkgs.screen
     pkgs.heroku
-    pkgs.less
     pkgs.nodejs-16_x
     pkgs.nano
     pkgs.python3
