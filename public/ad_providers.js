e1d1e89d0b27e40c3a4c50c8e6cf76a5ed23cb0d
PoolloverNathan <nathan.kulzer@protonmail.com>
2022-12-01 15:08:12 +0000

    ad placeholder

--- public/ad_providers.js
+++ public/ad_providers.js
@@ -3,7 +3,7 @@ const providers = {
     const img = document.createElement("img")
     img.src = "/adplaceholder.png"
     el.replaceChildren(img)
-  }
+  },
   none(el) {
     el.replaceChildren()
   }
@@ -11,6 +11,5 @@ const providers = {
 
 const apregex = /(?<=[?&])ads=(\w+)(?=&|$)/
 const default_ap = "placeholder"
-const prefer_ap = apregex.exec(location.search)?.[1] ?? "false"
-const apfunction = providers[prefer_ap] ?? providers[default_ap]
-alert(`ads from ${prefer_ap} (default ${default_ap})`)
+const prefer_ap = apregex.exec(location.search)?.[1] ?? default_ap
+const apfunction = providers[prefer_ap] ?? providers[default_ap];
